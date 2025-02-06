94c36c0650815bc5f7b7018c9ba1df1c4e1fcabc
kim-do-hyeon <pental@kakao.com>
2025-02-06 18:15:38 +0900

    Initial Commit

---
+++ argos-deepfake-guard/argos_deepfake_guardApp.swift
@@ -0,0 +1,17 @@
+//
+//  argos_deepfake_guardApp.swift
+//  argos-deepfake-guard
+//
+//  Created by pental on 2/6/25.
+//
+
+import SwiftUI
+
+@main
+struct argos_deepfake_guardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ argos-deepfake-guardTests/argos_deepfake_guardTests.swift
@@ -0,0 +1,17 @@
+//
+//  argos_deepfake_guardTests.swift
+//  argos-deepfake-guardTests
+//
+//  Created by pental on 2/6/25.
+//
+
+import Testing
+@testable import argos_deepfake_guard
+
+struct argos_deepfake_guardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
